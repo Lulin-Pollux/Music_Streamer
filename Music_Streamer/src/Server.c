064a671d877432287c72b38b5533197591949e3c
Lulin Pollux <35562421+Lulin-Pollux@users.noreply.github.com>
2018-10-30 16:36:44 +0900

    전체 재생목록 출력 함수 생성
    1. 전체 재생목록을 보여주는 기능의 코드를 함수로 만들었다.

--- Music_Streamer/src/Server.c
+++ Music_Streamer/src/Server.c
@@ -6,6 +6,25 @@
 #include "ClassLinker.h"
 
 
+//전체 재생목록을 출력한다.
+int printFullPlaylist(char playlist[][512])
+{
+	printf("\t 재생목록 \n");
+	printf("--------------------------------\n");
+	for (int i = 1; i < 100; i++)
+	{
+		if (strlen(playlist[i]) == 0)
+			break;
+		else
+		{
+			char *pos = strrchr(playlist[i], '/');
+			printf("%d. %s\n", i, pos + 1);
+		}
+	}
+
+	return 0;
+}
+
 //서버 메인 함수
 int server(SETTINGS sets)
 {
@@ -98,19 +117,8 @@ int server(SETTINGS sets)
 			break;
 		}
 
-		//재생목록을 모두 보여준다.
-		printf("\t재생목록 \n");
-		printf("--------------------------------\n");
-		for (int i = 1; i < 100; i++)
-		{
-			if (strlen(playlist[i]) == 0)
-				break;
-			else
-			{
-				char *pos = strrchr(playlist[i], '/');
-				printf("%d. %s\n", i, pos + 1);
-			}
-		}
+		//전체 재생목록을 출력한다.
+		printFullPlaylist(playlist);
 
 		//재생목록에 있는 모든 파일을 전송한다.
 		double allSendBytes = 0.0;
